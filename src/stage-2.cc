53a0889057a202401cb5926ad21e19702830c878
Henrique Silva <hcpsilva@inf.ufrgs.br>
2022-12-31 15:35:25 -0300

    cleanup: removed unneded files, main cleanup

--- src/stage-2.cc
+++ src/stage-2.cc
@@ -4,17 +4,15 @@
  * Este arquivo será posterioremente substituído, não acrescente nada.
  */
 
-#include <stdio.h>
-
 #include "driver.hh"
 
 int main(
     [[maybe_unused]] int argc,
     [[maybe_unused]] char** argv)
 {
-    hcpsilva::driver driver("");
+    hcpsilva::driver driver;
 
-    int ret = driver.parse("");
+    int ret = driver.parse();
 
     return ret;
 }
